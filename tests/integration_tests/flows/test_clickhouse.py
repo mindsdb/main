e8f85eb1159d326ecf40755218ab7e6a9a3ac1ac
Max Stepanov <stpmax@yandex.ru>
2021-05-06 14:23:41 +0000

    test

--- tests/integration_tests/flows/test_clickhouse.py
+++ tests/integration_tests/flows/test_clickhouse.py
@@ -121,6 +121,7 @@ class ClickhouseTest(unittest.TestCase):
         self.assertTrue(TEST_DATA_TABLE in self.get_tables_in('test_data'))
 
         mindsdb_tables = self.get_tables_in(MINDSDB_DATABASE)
+        print(f'TABLES {mindsdb_tables}')
         self.assertTrue(len(mindsdb_tables) >= 2)
         self.assertTrue('predictors' in mindsdb_tables)
         self.assertTrue('commands' in mindsdb_tables)
